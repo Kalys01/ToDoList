c1ed446392e91d5aa08d7a85d740885028572f3b
Kalys01 <kalysosmon96.g@gmail.com>
2021-01-07 01:14:02 +0600

    new pattern

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Kalys");
+let btn = document.getElementById('btn');
+let inpt = document.getElementById('inpt');
+let out = document.getElementById('out');
+let msg = [];
+
+
+
+btn.addEventListener('click', function(){
+    msg.push(message);
+    for(let i = 0; i < msg.length; i++){
+        let parags = document.createElement('p');
+            parags.innerHTML = msg.pop();
+            parags.className = `className-note${i+1}`;
+            document.body.appendChild(parags);
+    }
+})
+
+inpt.addEventListener('input', function(e){
+    message = e.target.value;
+})
+
